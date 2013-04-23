09e01e1e7d299160614b6155a329f1739da338c9
Dyeison Schreiner <dyeison.kut@gmail.com>
2013-04-22 21:07:38 -0300

    df

---
+++ cadastro.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("#enviar").click(function() {
+		var nome = $("#nome");
+		var nomePost = nome.val(); 
+		var email = $("#email");
+		var emailPost = email.val(); 
+		var senha = $("#senha");
+		var senhaPost = senha.val(); 	
+		$.post("cadastro.php", {nome: nomePost, email: emailPost, senha: senhaPost},
+		function(data){
+		 $("#resposta").html(data);
+		 }
+		 , "html");
+	});
+});
+
