ecbea1d2187d37f9a58507c58058642a86f8a164
Dyeison Schreiner <dyeison.kut@gmail.com>
2013-05-24 14:55:41 -0300

    j

--- tela-insere-questoes/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$HOST = "XwGo6Y94p8Sm.db.10856794.hostedresource.com";
-    $USER = "XwGo6Y94p8Sm";
-    $PASS = "as@quA00P7JcC";
-    $DATABASE = "XwGo6Y94p8Sm";
-    $con = mysql_connect($HOST, $USER, $PASS);
-    $db = mysql_select_db($DATABASE, $con);
-?>
