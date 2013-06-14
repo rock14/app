05953d4deeabf4b6057d613c9ede6e88a1554e5a
Dyeison Schreiner <dyeison.kut@gmail.com>
2013-06-14 19:15:09 -0300

    5

--- dados_js_php_grafico.js
+++ dados_js_php_grafico.js
@@ -3,13 +3,26 @@ erros = 0;
 tempo_estudado = 0;
 
 tp_direito_processual_civil = 0;
-tp_direito_administratico = 0;
+tp_direito_administrativo = 0;
 tp_direito_civil = 0;
 tp_matematica = 0;
 tp_raciocinio_logico = 0;
 tp_direito_constitucional = 0;
 
-				
+acertos_direito_processual_civil = 0;
+erros_direito_processual_civil = 0;
+acertos_direito_administrativo = 0;
+erros_direito_administrativo = 0;
+acertos_direito_civil = 0;
+erros_direito_civil = 0;
+acertos_matematica = 0;
+erros_matematica = 0;
+acertos_raciocinio_logico = 0;
+erros_raciocinio_logico = 0;
+acertos_direito_constitucional = 0;
+erros_direito_constitucional = 0;
+
+	
 function acerto_erros_total(){
 	var controle = 1;
 	$.post("http://www.aaconcursos.com/app/graficos.php", {controle: controle,username: username},
@@ -20,15 +33,54 @@ function acerto_erros_total(){
 				
 				acertos = 0;
 				erros = 0;
-								
-				for(var k=0; k<cont; k++){ //insere as questoes no javascript
-					acertos = (acertos + parseInt(arr[k].numeroAcertos));
-					erros = (erros + parseInt(arr[k].numeroErros));
-				}
-				console.log(acertos +" Acertos" );
-				console.log(erros +" Erros" );
+				acertos_direito_processual_civil = 0;
+				erros_direito_processual_civil = 0;
+				acertos_direito_administrativo = 0;
+				erros_direito_administrativo = 0;
+				acertos_direito_civil = 0;
+				erros_direito_civil = 0;
+				acertos_matematica = 0;
+				erros_matematica = 0;
+				acertos_raciocinio_logico = 0;
+				erros_raciocinio_logico = 0;
+				acertos_direito_constitucional = 0;
+				erros_direito_constitucional = 0;
 				
-				//desenha_total_acertos_erros();	
+				if(arr[0].acertos!=null)
+					acertos_direito_processual_civil = parseInt(arr[0].acertos);
+				if(arr[0].erros!=null)
+					erros_direito_processual_civil = parseInt(arr[0].erros);
+				
+				if(arr[1].acertos!=null)
+					acertos_direito_administrativo = parseInt(arr[1].acertos);
+				if(arr[1].erros!=null)
+					erros_direito_administrativo = parseInt(arr[1].erros);
+					
+				if(arr[2].acertos!=null)
+					acertos_direito_civil = parseInt(arr[2].acertos);
+				if(arr[2].erros!=null)
+					erros_direito_civil = parseInt(arr[2].erros);
+					
+				if(arr[3].acertos!=null)
+					acertos_matematica = parseInt(arr[3].acertos);
+				if(arr[3].erros!=null)
+					erros_matematica = parseInt(arr[3].erros);
+						
+				if(arr[4].acertos!=null)
+					acertos_raciocinio_logico = parseInt(arr[4].acertos);
+				if(arr[4].erros!=null)
+					erros_raciocinio_logico = parseInt(arr[4].erros);	
+					
+				if(arr[5].acertos!=null)
+					acertos_direito_constitucional = parseInt(arr[5].acertos);
+				if(arr[5].erros!=null)
+					erros_direito_constitucional = parseInt(arr[5].erros);
+				
+				
+				acertos = (acertos_direito_processual_civil + acertos_direito_administrativo + acertos_direito_civil + acertos_matematica + acertos_raciocinio_logico + acertos_direito_constitucional);
+				erros = (erros_direito_processual_civil + erros_direito_administrativo + erros_direito_civil + erros_matematica + erros_raciocinio_logico + erros_direito_constitucional);
+					
+					
 				desenha_desempenho_assunto();
 							
 		 }
@@ -49,7 +101,7 @@ function total_tempo_estudado(){
 				if(arr[0].tempo!=null)
 					tp_direito_processual_civil = parseInt(arr[0].tempo);
 				if(arr[1].tempo!=null)
-					tp_direito_administratico = parseInt(arr[1].tempo);
+					tp_direito_administrativo = parseInt(arr[1].tempo);
 				if(arr[2].tempo!=null)
 					tp_direito_civil = parseInt(arr[2].tempo);
 				if(arr[3].tempo!=null)
@@ -61,14 +113,7 @@ function total_tempo_estudado(){
 				
 	
 				desenha_tempo_estudado();
-				/*
-				for(var k=0; k<cont; k++){ //insere as questoes no javascript
-					if (arr[k].tempo!=null)
-						console.log("Total Tempo Estudado: "+ parseInt(arr[k].tempo) +" segundos.");
-				}
-				*/
-				
-									
+												
 		 }
 		 , "html");
 }
--- desenha_graficos.js
+++ desenha_graficos.js
@@ -1,18 +1,25 @@
 function handler_grafico_desempenho(){
-	if(username=="[usuário não logado]")
-		alert("usuário não logado!");
+	if(username=="[usuário não logado]"){
+		console.log("Usuário não logado!");
+		
+		// alert phonegap  (message, callback, title, buttonName)
+		navigator.notification.alert('Usuário não logado!', fcalert, 'Comunicado', 'OK');
+	}
 	else
 		window.location = "#grafico";
 	
 }
 
+function fcalert(){
+	
+}
 
 // Column Chart
 function desenha_tempo_estudado() {
 	var data = google.visualization.arrayToDataTable([
 	  ['Assunto', 'Tempo Estudado'],
 	  ['Direito Processual Civil',  tp_direito_processual_civil],
-	  ['Direito Administrativo',  tp_direito_administratico],
+	  ['Direito Administrativo',  tp_direito_administrativo],
 	  ['Direito Civil',  tp_direito_civil],
 	  ['Matemática',  tp_matematica],
 	  ['Raciocínio Lógico',  tp_raciocinio_logico],
@@ -20,7 +27,7 @@ function desenha_tempo_estudado() {
 	]);
 	
 	var options = {
-	  title: 'Tempo Estudado por Assunto',
+	  title: 'Tempo Estudado por Assunto (em segundos)',
 	  colors: ['green'],
 	  hAxis: {title: 'Assunto', titleTextStyle: {color: 'red'}}
 	};
@@ -35,12 +42,12 @@ function desenha_desempenho_assunto() {
     
 	var data = google.visualization.arrayToDataTable([
 	  ['Assunto', acertos+' Acertos', erros+' Erros'],
-	  ['Direito Processual Civil',  1000,      400],
-	  ['Direito Administrativo',  1170,      460],
-	  ['Direito Civil',  660,       1120],
-	  ['Matemática',  1030,      540],
-	  ['Raciocínio Lógico',  1030,      540],
-	  ['Direito Constitucional',  1030,      540],
+	  ['Direito Processual Civil',  acertos_direito_processual_civil, erros_direito_processual_civil],
+	  ['Direito Administrativo',  acertos_direito_administrativo, erros_direito_administrativo],
+	  ['Direito Civil',  acertos_direito_civil, erros_direito_civil],
+	  ['Matemática',  acertos_matematica, erros_matematica],
+	  ['Raciocínio Lógico',  acertos_raciocinio_logico, erros_raciocinio_logico],
+	  ['Direito Constitucional',  acertos_direito_constitucional, erros_direito_constitucional],
 	]);
 
 	var options = {
